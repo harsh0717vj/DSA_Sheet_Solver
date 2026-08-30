a9677552354d4df29b96595faf536639205f029d
Harsh Vardhan <hcvardhan0917@gmail.com>
2026-08-30 22:31:28 +0530

    Implement solution for First Missing Positive problem

---
+++ Arrays/Leetcode41. First Missing Positive/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        while(i<n){
+            if(nums[i]>=1&&nums[i]<=n&&nums[nums[i]-1]!=nums[i]) swap(nums[i],nums[nums[i]-1]);
+            else i++;
+        }
+        for(int j=0;j<n;j++){
+            if(nums[j]!=j+1) return j+1;
+        }
+        return n+1;
+    }
+};
