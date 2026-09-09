738879f45080323ffc5a60955e968908a9b32730
Harsh Vardhan <hcvardhan0917@gmail.com>
2026-09-09 22:49:01 +0530

    Add solution for Leetcode 135: Candy problem
    Implement candy distribution algorithm based on ratings.

---
+++ Greedy_Algorithm/Leetcode135. Candy/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>candy(n);
+        for(int i=0;i<n;i++){
+            candy[i]=1;
+        }
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]) candy[i]=candy[i-1]+1;
+        }
+        for(int j=n-2;j>=0;j--){
+            if(ratings[j]>ratings[j+1]) candy[j]=max(candy[j],candy[j+1]+1);
+        }
+        int minCandy=0;
+        for(int i=0;i<n;i++){
+            minCandy+=candy[i];
+        }
+        return minCandy;
+    }
+};
