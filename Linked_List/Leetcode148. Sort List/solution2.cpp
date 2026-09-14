6c0387362b1a0b555bf2aa4fa0773115a1c59af7
Harsh Vardhan <hcvardhan0917@gmail.com>
2026-09-14 23:04:30 +0530

    Solved using slow and fast pointer and merge sort

---
+++ Linked_List/Leetcode148. Sort List/solution2.cpp
@@ -0,0 +1,37 @@
+class Solution{
+public:
+ListNode*merge(ListNode*a,ListNode*b){
+    ListNode*c=new ListNode(0);
+    ListNode*temp=c;
+    while(a!=NULL&&b!=NULL){
+        if(a->val<=b->val){
+            temp->next=a;
+            a=a->next;
+        }
+        else{
+            temp->next=b;
+            b=b->next;
+        }
+        temp=temp->next;   
+    }
+    if(a!=NULL) temp->next=a;
+        else temp->next=b;
+        return c->next;
+}
+    ListNode*sortList(ListNode*head){
+        if(head==NULL||head->next==NULL) return head;
+       ListNode*slow=head;
+       ListNode*fast=head;
+       while(fast->next!=NULL&&fast->next->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+       } 
+       ListNode*a=head;
+       ListNode*b=slow->next;
+       slow->next=NULL;
+       a=sortList(a);
+       b=sortList(b);
+       ListNode*C=merge(a,b);
+       return C;
+    }
+};
