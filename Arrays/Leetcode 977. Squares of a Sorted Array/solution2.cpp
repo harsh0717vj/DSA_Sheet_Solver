9258af56f9bbcd94a3261351d1f8b36784290f3a
Harsh Vardhan <hcvardhan0917@gmail.com>
2026-09-25 23:30:56 +0530

    Solved using Three Pointers

---
+++ Arrays/Leetcode 977. Squares of a Sorted Array/solution2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
+        int k=n-1;
+        vector<int>ans(n);
+        while(i<=j){
+            if(abs(nums[i])>abs(nums[j])) {
+                ans[k]=nums[i]*nums[i];
+                i++;
+            }
+            else{
+                ans[k]=nums[j]*nums[j];
+                j--;
+            }
+            k--;
+        }
+        return ans;
+    }
+};
